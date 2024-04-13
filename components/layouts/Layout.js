f6d543cdbf66b230d8eb224cf3ae6a30bbf4aa77
Javad Bahrami <javadev14bh@gmail.com>
2024-04-13 14:55:56 +0330

    Add layouts and global styles

---
+++ components/layouts/Layout.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import styles from "./Layout.module.css";
+
+function Layout({ children }) {
+  return (
+    <>
+      <header className={styles.header}>
+        <Link href="/">
+          <h2>NEXTCAR</h2>
+          <p>Choose and buy your car!</p>
+        </Link>
+      </header>
+      <main className={styles.container}>{children}</main>
+      <footer className={styles.footer}>
+        {" "}
+        <p>
+          Developed by{" "}
+          <a target="_blank" href="https://github.com/javadevbh/">
+            Javad
+          </a>{" "}
+          with ❤️ | Next.js project &copy;
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default Layout;
